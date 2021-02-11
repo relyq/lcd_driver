f5c066133850fccf8d8dcd213f968979570421b3
relyq <nicoale_mainenti@hotmail.es>
2021-02-11 07:08:01 -0300

    done

--- 8bit-11pins/main.c
+++ 8bit-11pins/main.c
@@ -8,8 +8,10 @@ int main(void) {
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
 
   LCD_init();
-  LCD_puts("sogui<3");
+  LCD_puts("123");
   LCD_command(LCD_CURSOR);
+  LCD_cursor(1, 0);
+  LCD_puts("456");
 
   while (1) {
     sleep_mode();
